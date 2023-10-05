2cb7258d396327db88a2a93e2e702ff24f3f04e6
swati2304 <swatinimje23@gmail.com>
2023-10-05 22:53:23 +0530

    changes added

---
+++ src/components/contactCard/contactCard.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "./contactCard.css"
+
+export default function ContactCard({name, mobile, email}){
+    return(
+        <div className="contact-card">
+            <p className="contact-name m-2">🐼 {name} </p>
+            <p className="contact-mobile m-2"> 📱{mobile} </p>
+            <p className="contact-email m-2">📧 {email} </p>
+        </div>
+    )
+}
--- src/views/Home/home.js
+++ src/views/Home/home.js
@@ -1,8 +1,9 @@
 import "./home.css"
+import ContactCard from "../../components/contactCard/contactCard";
 import React, {useState} from "react"
+import showToast from 'crunchy-toast';
 
 export default function Home(){
-
     const [contacts, setContancts] = useState([
         {
             name:"Swati",
@@ -25,6 +26,27 @@ export default function Home(){
             email:"avikshanimje23@gmail.com"
         }
     ]);
+
+    const [name, setname] = useState('');
+    const [email, setemail] = useState('');
+    const [mobile, setmobile] = useState('');
+
+    const addContact = ()=>{
+        const obj = {
+            name: name,
+            email: email,
+            mobile: mobile
+        }
+
+        setContancts([...contacts, obj]);
+
+        showToast('Contact added successfully', 'success', 3000);
+
+        setname('');
+        setemail('');
+        setmobile('');
+    }
+
     return(
         <div>
             <h1 className="app-title"> 📞Contact App</h1>
@@ -32,9 +54,47 @@ export default function Home(){
             <div className="app-body">
                 <div className="contacts-container">
                     <h2 className="sub-heading">Show Contacts</h2>
+                    {
+                        contacts.map((contacts, index)=>{
+                            const {name, mobile, email}= contacts;
+                            return( <ContactCard key={index}
+                                name={contacts.name} 
+                                mobile={contacts.mobile}
+                                email={contacts.email}/>)
+                        })
+                    }
                 </div>
                 <div className="contacts-container">
                     <h2 className="sub-heading">Add Contacts</h2>
+                    <form>
+                        <input type="text" 
+                        placeholder="Name"  
+                        className="user-input"
+                        onChange={(e)=>{
+                            setname(e.target.value)
+                        }}
+                        value={name}/>
+                        
+                        <input type="email"
+                        placeholder="Email"
+                        className="user-input"
+                        onChange={(e)=>{
+                            setemail(e.target.value)
+                        }}
+                        value={email}/>
+                        
+                        <input type="text" 
+                        placeholder="Mobile" 
+                        className="user-input"
+                        onChange={(e)=>{
+                            setmobile(e.target.value)
+                        }}
+                        value={mobile}/>
+                        
+                        <button type="button" className="btn-add-contact"
+                        onClick={addContact}>
+                        Add Contact</button>
+                    </form>
                 </div>
             </div>
         </div>
